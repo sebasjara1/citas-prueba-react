a2e3275973bc2721ac02e59a34e750eff61a8f39
sebasjara1 <j.jaramillo6@pascualbravo.edu.co>
2020-07-05 22:07:57 -0500

    proyecto terminado

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { Fragment, useState, useEffect } from "react";
+import Formulario from "./components/Formulario";
+import Cita from "./components/Cita";
+
+function App() {
+  let citasIniciales = JSON.parse(localStorage.getItem("citas"));
+
+  if (!citasIniciales) {
+    citasIniciales = [];
+  }
+
+  const [citas, guardarCitas] = useState(citasIniciales);
+
+  useEffect(() => {
+    if (citasIniciales) {
+      localStorage.setItem("citas", JSON.stringify(citas));
+    } else {
+      localStorage.setItem("citas", JSON.stringify([]));
+    }
+
+  }, [citas, citasIniciales]);
+
+  const crearCita = (cita) => {
+    guardarCitas([...citas, cita]);
+  };
+
+  const eliminarCita = (id) => {
+    const nuevasCitas = citas.filter((cita) => cita.id !== id);
+    guardarCitas(nuevasCitas);
+  };
+
+  const titulo = citas.length === 0 ? "No hay citas" : "Administrar Citas";
+
+  return (
+    <Fragment>
+      <h1>Administrador de Pacientes</h1>
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario crearCita={crearCita} />
+          </div>
+          <div className="one-half column">
+            <h2>{titulo}</h2>
+            {citas.map((cita) => (
+              <Cita key={cita.id} cita={cita} eliminarCita={eliminarCita} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,109 @@
+import React, { Fragment, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+const Formulario = ({ crearCita }) => {
+  const [cita, actualizarCita] = useState({
+    mascota: "",
+    propietario: "",
+    fecha: "",
+    hora: "",
+    sintomas: "",
+  });
+
+  const [error, actualizarError] = useState(false);
+
+  const actualizarState = (e) => {
+    actualizarCita({
+      ...cita,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const { mascota, propietario, fecha, hora, sintomas } = cita;
+
+  const submitCita = (e) => {
+    e.preventDefault();
+
+    if (
+      mascota.trim() === "" ||
+      propietario.trim() === "" ||
+      fecha.trim() === "" ||
+      hora.trim() === "" ||
+      sintomas.trim() === ""
+    ) {
+      actualizarError(true);
+      return;
+    }
+
+    actualizarError(false);
+
+    cita.id = uuidv4();
+
+    crearCita(cita);
+
+    actualizarCita({
+      mascota: "",
+      propietario: "",
+      fecha: "",
+      hora: "",
+      sintomas: "",
+    });
+  };
+
+  return (
+    <Fragment>
+      <h2>Crear Cita</h2>
+      {error ? (
+        <p className="alerta-error">Todos los campos son obligatorios</p>
+      ) : null}
+      <form onSubmit={submitCita}>
+        <label>Nombre Mascotas</label>
+        <input
+          type="text"
+          name="mascota"
+          className="u-full-width"
+          placeholder="Nombre mascota"
+          onChange={actualizarState}
+          value={mascota}
+        />
+        <label>Nombre Dueño</label>
+        <input
+          type="text"
+          name="propietario"
+          className="u-full-width"
+          placeholder="Nombre dueño"
+          onChange={actualizarState}
+          value={propietario}
+        />
+        <label>Fecha</label>
+        <input
+          type="date"
+          name="fecha"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={fecha}
+        />
+        <label>Hora</label>
+        <input
+          type="time"
+          name="hora"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={hora}
+        />
+        <label>Síntomas</label>
+        <textarea
+          name="sintomas"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={sintomas}
+        ></textarea>
+        <button type="submit" className="u-full-width button-primary">
+          Agregar Cita
+        </button>
+      </form>
+    </Fragment>
+  );
+};
+
+export default Formulario;
