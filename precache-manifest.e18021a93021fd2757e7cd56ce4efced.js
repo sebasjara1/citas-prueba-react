49dc202a3946b5306072ad4f78491adf05b6c2c7
sebasjara1 <j.jaramillo6@pascualbravo.edu.co>
2020-07-05 22:06:20 -0500

    Updates

---
+++ precache-manifest.e18021a93021fd2757e7cd56ce4efced.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16677eb830bb22967715f6fc20da4f28",
+    "url": "/citas-prueba-react/index.html"
+  },
+  {
+    "revision": "f95e170c8d5cef701541",
+    "url": "/citas-prueba-react/static/css/main.635bd718.chunk.css"
+  },
+  {
+    "revision": "0012f5a03a153668762a",
+    "url": "/citas-prueba-react/static/js/2.2aca169f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/citas-prueba-react/static/js/2.2aca169f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f95e170c8d5cef701541",
+    "url": "/citas-prueba-react/static/js/main.e2921190.chunk.js"
+  },
+  {
+    "revision": "5b3a13f93a06926995c1",
+    "url": "/citas-prueba-react/static/js/runtime-main.ce2f0c31.js"
+  }
+]);
